76622479162baedbb1359cab03bf09f8d6168b5a
Adi Gonnen <adigonnen@gmail.com>
2018-09-12 14:19:12 +0300

    initilize

---
+++ src/components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>nadlan</h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/router.js
+++ src/router.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+// import About from './views/About.vue'
+// import Contact from './views/Contact.vue'
+// import HouseDetails from './views/HouseDetails.vue'
+
 
 Vue.use(Router)
 
@@ -11,13 +15,25 @@ export default new Router({
       name: 'home',
       component: Home
     },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
+    // {
+    //   path: '/login',
+    //   name: 'login',
+    //   component: Login
+    // },
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   component: About
+    // },
+    // {
+    //   path: '/contact',
+    //   name: 'contact',
+    //   component: Contact
+    // },
+    // {
+    //   path: '/house/:houseId?',
+    //   name: 'house-details',
+    //   component: HouseDetails
+    // },
   ]
-})
+  })
