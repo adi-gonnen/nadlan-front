bdf94a8ca7e6c2cf1416ccb255fe9af5e9b4ff77
Adi Gonnen <adigonnen@gmail.com>
2018-09-12 14:42:28 +0300

    add json house

---
+++ src/Services/HouseService.js
@@ -0,0 +1,16 @@
+[{
+    price: 2030000,
+    parking: true,
+    sunBalcony: true,
+    storage: true,
+    elevator: false,
+    securityRoom: false,
+    category: 4,
+    floor: 2/3,
+    size: 112,
+    en: {
+        title: "nice 4 rooms in Raanana center",
+        desc: "very nice appartment, excellent for investment.",
+        city: "Ra'anana"
+    }
+}]
