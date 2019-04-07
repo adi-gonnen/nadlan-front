227c06065b6360a6ca7fe8cd5354646434ea09a9
Adi Gonnen <adigonnen@gmail.com>
2019-04-07 23:06:38 +0300

    test

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,31 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1 class="about-title">Sarit Shacham</h1>
+    <h4 class="sub-title">From IDF to a Real Estate Mentor</h4>
+    <p class="body-sarit">
+      I served in the IDF for 20 years which allowed me to raise my family as I was home every day around 3pm.  It was an amazing career but after some time I felt there was a need for a change and to start a new chapter of my life.  I retired from the IDF to start a business of my own.
+      <br/>
+      I managed my own beauty business of 8 employees and after a few years I decided to close my business to try out the workforce as a regular employee again.
+      After trying a few opportunities, I found that I excelled as an insurance agent and loved doing for six years.  Again I decided to try go out on my own and try something new.
+      <br/>
+      One Shabbat dinner at my son in law’s mother asked me, "Why don’t you go to real estate? It really suits you! "
+      This is how I found my path ...
+      <br/>
+      I joined Remax agency in Ramat Gan, and the rest, as they say ... history.
+      In the first year of my work as a real estate consultant at the Agency, I received a certificate of appreciation for the amount of sales, and I was awarded the title of "The President Club". Three years later I decided to combine the two loves of my life: the world of sales - and the world of training. I accompanied real estate consultants at every stage of their careers. From those who are debating whether the field suits them to those who want to build a team that will help them in career advancement. During the year 2017, I registered another achievement and published the book, "A winning recipe for real estate consultant". This is the first book in Israel that provides a professional solution for anyone who wants to turn the consultation into a profession.
+      <br/>
+      Today I accompany businesses in general and real estate in particular, and raise them on the main road. for further details, please call: 054-5222395
+    </p>
   </div>
 </template>
+
+<style>
+.body-sarit {
+  width: 70%;
+  margin: auto;
+  text-align: justify;
+}
+.title-sarit {
+  
+}
+</style>
